d7116be3cced269fd2bb1f0ec3ff49ec7c53594f
Alex Carpenter <alex@mightyinthemidwest.com>
2017-01-26 18:26:39 -0500

    Updated design

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ ruby RUBY_VERSION
 gem "jekyll", "3.2.1"
 gem "jekyll-gist"
 gem "jemoji"
+gem "jekyll-seo-tag"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 # gem "minima"
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+let webpack = require('webpack');
+let path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/assets/js/app.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public/assets/js'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: [ ]
+    })
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourcemap: true,
+      compress: {
+        warnings: false
+      }
+    })
+  );
+
+  module.exports.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  );
+}
