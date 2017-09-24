3d99edba791a75d4b7591651e65edfac665d3158
Alex Carpenter <alex@mightyinthemidwest.com>
2017-09-24 18:12:36 -0400

    Adds keypress function to skip-to.js

--- src/javascripts/modules/skipTo.js
+++ src/javascripts/modules/skipTo.js
@@ -1,17 +1,45 @@
 export default function () {
   const skipTo = document.querySelector('[data-skip-to]')
+  const skipToLinks = skipTo.children
+  const skipToFirst = skipToLinks[0]
+  const skipToLast = skipToLinks[skipToLinks.length - 1]
+  const keyCodes = {
+    upArrow: 40,
+    downArrow: 38
+  }
 
   if (skipTo) {
-    const skipToLinks = skipTo.children
+    skipTo.addEventListener('click', focusElement)
+    skipTo.addEventListener('keydown', bindKeyPress)
+  }
 
-    skipTo.addEventListener('click', (event) => {
-      const target = event.target
-      const id = target.getAttribute('href').slice(1)
-      const targetElement = document.getElementById(id)
+  function focusElement (event) {
+    const target = event.target
+    const id = target.getAttribute('href').slice(1)
+    const targetElement = document.getElementById(id)
 
-      if (targetElement) {
-        targetElement.focus()
+    if (targetElement) {
+      targetElement.focus()
+    }
+  }
+
+  function bindKeyPress (event) {
+    const eventWhich = event.which
+    const eventTarget = event.target
+
+    if (eventWhich === keyCodes.upArrow || eventWhich === keyCodes.downArrow) {
+      event.preventDefault()
+
+      switch (eventWhich) {
+        case keyCodes.upArrow:
+          const skipToLastActive = eventTarget === skipToLast
+          skipToLastActive ? skipToFirst.focus() : eventTarget.nextElementSibling.focus()
+          break
+        case keyCodes.downArrow:
+          const skipToFirstActive = eventTarget === skipToFirst
+          skipToFirstActive ? skipToLast.focus() : eventTarget.previousElementSibling.focus()
+          break
       }
-    })
+    }
   }
 }
