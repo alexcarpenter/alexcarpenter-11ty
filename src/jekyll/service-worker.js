7b815eeca9fe0842e33cad440691691ced6330c6
Alex Carpenter <alex@mightyinthemidwest.com>
2017-09-24 17:29:57 -0400

    Updates service worker version

--- src/jekyll/service-worker.js
+++ src/jekyll/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const version = 'v1.2::';
+const version = 'v1.3::';
 const staticCacheName = version + 'static';
 const pagesCacheName = version + 'pages';
 const imagesCacheName = version + 'images';
