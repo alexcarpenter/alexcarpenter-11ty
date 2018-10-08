b66da3eb59b2657eeec1e264680da223c3d8b8d6
Alex Carpenter <alex@mightyinthemidwest.com>
2018-10-08 16:46:37 -0400

    Centers page

--- src/_includes/components/Figure.js
+++ src/_includes/components/Figure.js
@@ -1,5 +1,5 @@
-module.exports = ({src, alt = '', caption = '', ratio = '16/9'}) => `
-  <figure>
+module.exports = ({ src, alt = '', caption = '', ratio = '16/9' }) => `
+  <figure class="u-extend">
     <div style="--aspect-ratio: ${ratio};">
       <img src="${src}" alt="${alt}" />
     </div>
