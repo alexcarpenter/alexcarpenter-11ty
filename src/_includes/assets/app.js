d98be0fea6cca73a2801cf131b361b07bb5ffb2e
Alex Carpenter <alex@mightyinthemidwest.com>
2018-11-12 21:52:41 -0500

    V5

---
+++ src/_includes/assets/app.js
@@ -0,0 +1,25 @@
+(function() {
+  var nav = document.querySelector('.js-nav');
+
+  document.addEventListener('click', function(e) {
+    toggleNav(e);
+  }, false);
+
+  function toggleNav(e) {
+    if (!e.target.matches('.js-nav-toggle')) return;
+    var btn = e.target;
+    btn.classList.contains('is-active') ? hideNav(btn) : showNav(btn);
+  }
+
+  function showNav(el) {
+    el.classList.add('is-active');
+    nav.classList.add('is-visible');
+    el.setAttribute('aria-expanded', true);
+  }
+
+  function hideNav(el) {
+    el.classList.remove('is-active');
+    nav.classList.remove('is-visible');
+    el.setAttribute('aria-expanded', false);
+  }
+})();
