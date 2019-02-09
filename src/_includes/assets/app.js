ba546c827b80ee9e6fccb13e425df2bce5910d8e
Alex Carpenter <im.alexcarpenter@gmail.com>
2019-02-09 10:19:09 -0500

    Adds page timing

--- src/_includes/assets/app.js
+++ src/_includes/assets/app.js
@@ -22,4 +22,23 @@
     nav.classList.remove('is-visible');
     el.setAttribute('aria-expanded', false);
   }
+
+  // Ref: https://timkadlec.com/
+  window.onload = function(){
+    setTimeout(function(){
+      window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
+      var t = performance.timing || {};
+
+      if (!t) {
+        
+        return;
+      }
+      var start = t.navigationStart,
+          end = t.loadEventEnd
+          loadTime = (end - start) / 1000;
+
+      var copy = document.querySelectorAll('.js-page-timing');
+      copy[0].innerHTML += "<p class='loaded'>This page loaded in <strong>" + loadTime + " seconds</strong>.</p>";
+    }, 0); 
+  }
 })();
