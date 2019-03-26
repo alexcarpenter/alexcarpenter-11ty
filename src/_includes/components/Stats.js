0bf1815df39aaf04738b98c802cdf7ca32d79108
Alex Carpenter <im.alexcarpenter@gmail.com>
2019-03-26 08:53:59 -0400

    New note

--- src/_includes/components/Stats.js
+++ src/_includes/components/Stats.js
@@ -1,2 +1,8 @@
 module.exports = ({ items = [] }) => {
+  return `<dl class="c-stats">
+${items.map((item) => `
+<dt class="c-stats__title">${item.title}</dt>
+<dd class="c-stats__description">${item.description}</dd>
+`).join('')}
+</dl>`;
 }
