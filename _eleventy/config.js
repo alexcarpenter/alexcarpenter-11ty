006fcc9f7d28123ca0d40413a3ccebd27c61adc8
Alex Carpenter <alex.carpenter@nationbuilder.com>
2019-12-06 17:56:02 -0500

    Minify HTML

--- _eleventy/config.js
+++ _eleventy/config.js
@@ -1,41 +1,44 @@
-const filters = require('./filters')
-const shortcodes = require('./shortcodes')
-const pluginRss = require('@11ty/eleventy-plugin-rss')
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const filters = require('./filters');
+const shortcodes = require('./shortcodes');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
   // Filters
   Object.keys(filters).forEach(filterName => {
-    eleventyConfig.addFilter(filterName, filters[filterName])
-  })
+    eleventyConfig.addFilter(filterName, filters[filterName]);
+  });
 
   // Shortcodes
   Object.keys(shortcodes).forEach(shortCodeName => {
-    eleventyConfig.addShortcode(shortCodeName, shortcodes[shortCodeName])
-  })
+    eleventyConfig.addShortcode(shortCodeName, shortcodes[shortCodeName]);
+  });
 
   // Plugins
-  eleventyConfig.addPlugin(pluginRss)
-  eleventyConfig.addPlugin(syntaxHighlight)
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   // Collections
   eleventyConfig.addCollection('posts', collection => {
-    return collection.getFilteredByGlob('**/posts/*.md').reverse()
-  })
+    return collection.getFilteredByGlob('**/posts/*.md').reverse();
+  });
 
   eleventyConfig.addCollection('screencasts', collection => {
-    return collection.getFilteredByGlob('**/screencasts/*.md').reverse()
-  })
+    return collection.getFilteredByGlob('**/screencasts/*.md').reverse();
+  });
 
   eleventyConfig.addCollection('work', collection => {
-    return collection.getFilteredByGlob('**/work/*.md').reverse()
-  })
+    return collection.getFilteredByGlob('**/work/*.md').reverse();
+  });
+
+  // Transforms
+  eleventyConfig.addTransform('htmlmin', filters.htmlmin);
 
   // ETC.
   eleventyConfig
     .addPassthroughCopy('src/assets')
     .addPassthroughCopy('src/manifest.json')
-    .addPassthroughCopy('src/_redirects')
+    .addPassthroughCopy('src/_redirects');
 
   return {
     templateFormats: ['njk', 'md', 'html'],
@@ -48,6 +51,6 @@ module.exports = function(eleventyConfig) {
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
-    passthroughFileCopy: true
-  }
-}
+    passthroughFileCopy: true,
+  };
+};
