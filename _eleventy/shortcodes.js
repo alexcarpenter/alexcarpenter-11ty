6e6102b5340a65d1c8a71f71c6c737cafa4f5c15
Alex Carpenter <alex.carpenter@nationbuilder.com>
2019-12-08 19:49:16 -0500

    Add external option

--- _eleventy/shortcodes.js
+++ _eleventy/shortcodes.js
@@ -11,17 +11,22 @@ const markdown = require('markdown-it')({
 const slugify = require('@sindresorhus/slugify');
 
 module.exports = {
-  Figure: function({ src, alt = '', caption = '', ratio = '16/9', breakout = false, lazyload = false }) {
+  Figure: function({
+    src,
+    alt = '',
+    caption = '',
+    ratio = '16/9',
+    breakout = false,
+    lazyload = false,
+  }) {
     const img = html`
       <figure>
         <div style="--aspect-ratio: ${ratio};">
-          <img src="${src}" alt="${alt}"${lazyload ? ` loading="lazy"` : ''} />
+          <img src="${src}" alt="${alt}" ${lazyload ? ` loading="lazy"` : ''} />
         </div>
-        ${
-          caption
-            ? `<figcaption class="u-text-center">${markdown.renderInline(caption)}</figcaption>`
-            : ''
-        }
+        ${caption
+          ? `<figcaption class="u-text-center">${markdown.renderInline(caption)}</figcaption>`
+          : ''}
       </figure>
     `;
     return html`
@@ -121,23 +126,13 @@ module.exports = {
     `;
   },
 
-  Link: function(url, text, svg) {
+  Link: function(url, text, external = false) {
     return html`
-      <a href="${url}" class="u-link">
+      <a href="${url}" class="u-link" ${external ? 'rel="external"' : ''}>
         ${text ? text : url.slice(url.indexOf(':') + 3)}
-        ${svg
-          ? svg
-          : `<svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            width="1em"
-            height="1em"
-            aria-hidden="true"
-            viewBox="0 0 24 24"
-          >
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
-          </svg>`}
+        ${external
+          ? `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" focusable="false" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>`
+          : `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" /></svg>`}
       </a>
     `;
   },
