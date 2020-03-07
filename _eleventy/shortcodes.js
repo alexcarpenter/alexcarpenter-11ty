f4651e6cece9680402e8b0ef9a94d460cb569319
Alex Carpenter <alex.carpenter@nationbuilder.com>
2020-03-07 08:47:26 -0500

    Tweaks

--- _eleventy/shortcodes.js
+++ _eleventy/shortcodes.js
@@ -46,7 +46,7 @@ module.exports = {
         <p>
           <span class="c-note__label${labelHidden ? ' u-hidden-visually' : ''}">${label}:</span>
           ${markdown.renderInline(text)}${link
-            ? `<br><a class="u-link" href="${link.url}">${link.text} <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="1em" height="1em" aria-hidden="true"viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg></a>`
+            ? `<br><a class="u-link" href="${link.url}">${link.text} <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" /></svg></a>`
             : ''}
         </p>
       </div>
